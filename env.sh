d76e15cb82ffbe3ab06fdf62913581900d7fb823
hbutler97 <hbutler@altera.com>
2016-08-04 21:16:18 -0700

    first rev of environment setup script

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export CROSS_COMPILE=/data/hbutler/socfpga-buildroot/toolchain/bin/arm-linux-gnueabihf-
+export ARCH=arm
+export OUT_DIR=/data/hbutler/socfpga-buildroot/socfpga-4.1.22-ltsi
